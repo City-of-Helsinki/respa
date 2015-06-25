388279198a76be0657855884169e69905bfb8196
Jussi Arpalahti <jussi.arpalahti@gmail.com>
2015-06-25 20:56:20 +0300

    Couple of ORM parameter fixes

--- resources/models.py
+++ resources/models.py
@@ -2,11 +2,10 @@ from django.utils import timezone
 from django.db import models
 from django.conf import settings
 
-
 class ModifiableModel(models.Model):
-    created_at = models.DateTimeField(default=timezone.now())
+    created_at = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, related_name="%(class)s_created")
-    modified_at = models.DateTimeField(default=timezone.now())
+    modified_at = models.DateTimeField(default=timezone.now)
     modified_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, related_name="%(class)s_modified")
 
     class Meta:
@@ -47,9 +46,17 @@ class Period(models.Model):
     resource = models.ForeignKey(Resource, db_index=True, related_name='periods')
     start = models.DateTimeField()
     end = models.DateTimeField()
-    name = models.CharField()
+    name = models.CharField(max_length="200")
     closed = models.BooleanField(default=False)
 
+DAYS_OF_WEEK = [
+    (1, 'maanantai'),
+    (2, 'tiistai'),
+    (3, 'keskiviikko'),
+    (4, 'torstai'),
+    (5, 'perjantai'),
+    (6, 'lauantai'),
+    (7, 'sunnuntai')]
 
 class Day(models.Model):
     """
@@ -58,7 +65,7 @@ class Day(models.Model):
     Kirjastot.fi API uses closed for both days and periods, don't know which takes precedence
     """
     period = models.ForeignKey(Period, db_index=True, related_name='days')
-    weekday = models.IntegerField("Day of week as a number 1-7", choices=[1, 2, 3, 4, 5, 6, 7])
+    weekday = models.IntegerField("Day of week as a number 1-7", choices=DAYS_OF_WEEK)
     opens = models.IntegerField("Clock as number, 0000 - 2359")
     closes = models.IntegerField("Clock as number, 0000 - 2359")
     closed = models.NullBooleanField(default=False)  # NOTE: If this is true and the period is false, what then?
