37ee5470f91cd71603096e4fed40985ba6303a3f
Jussi Arpalahti <jussi.arpalahti@gmail.com>
2015-06-25 22:38:04 +0300

    Test function to get active period for given time No logic yet for open and closed state since this depends on day settings, too, and needs conversion from dates to weekdays

--- resources/views.py
+++ resources/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Resource
+import django.db.models as dbm
 
 # Create your views here.
+
+
+def testink(start_date, end_date):
+    """
+    Testing various ways of getting to resources by their availability time
+    This function gets you active period for all resources in given date range
+
+    TODO: filter depending on closed state, but this depends on weekday state as well and this needs additional work
+
+    :param start_date, end_date:
+    :return: most specific Resource
+    """
+    res = Resource.objects.filter(
+        periods__start__lte=start_date, periods__end__gte=end_date).annotate(
+        periods_length=dbm.F('periods__end')-dbm.F('periods__start')
+    ).order_by('periods_length').first()
+    return res
