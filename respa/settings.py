43148cbaefd28ab7e5c8e3acc1b7c450f54031cb
Jussi Arpalahti <jussi.arpalahti@gmail.com>
2015-06-25 22:00:12 +0300

    Postgres settings

--- respa/settings.py
+++ respa/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'respa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'respa',
     }
 }
 
