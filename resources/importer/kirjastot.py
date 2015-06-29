4b3cd54994a9e8e81e5e44a4627fc470b6688938
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-06-29 15:49:48 +0300

    Kirjastot.fi importer seemingly functioning Also, admin now shows periods for units

--- resources/importer/kirjastot.py
+++ resources/importer/kirjastot.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 
 from .base import Importer, register_importer
 from ..models import Unit, UnitIdentifier, Period
@@ -17,8 +18,35 @@ class KirjastoImporter(Importer):
 
         data = [{'id': 'H53', 'periods': []}]
 
+        import json
+        s = json.load(open('kirdata.json'))
+        data = [s.pop()]
+
         for unit_data in data:
-            id_qs = UnitIdentifier.objects.filter(namespace='helmet', value=unit_data['id'])
+            id_qs = UnitIdentifier.objects.filter(namespace='helmet', value=unit_data['identificator'])
             unit = Unit.objects.get(identifiers=id_qs)
-            print(unit)
-            periods = unit.periods.all()
+            print("que", unit)
+            for period in unit_data['periods']:
+                start = datetime.datetime.strptime(period['start'], '%Y-%m-%d')
+                end = datetime.datetime.strptime(period['end'], '%Y-%m-%d')
+                active_period, created = unit.periods.get_or_create(
+                    start=start,
+                    end=end,
+                    description=period['description']['fi'],
+                    closed=period['closed'],
+                    name=period['name']['fi']
+                )
+                for day_id, day in period['days'].items():
+                    try:
+                        opens = int(day['opens'].replace(':', ''))
+                        closes = int(day['closes'].replace(':', ''))
+                    except ValueError:
+                        # Either hours missing or corrupt
+                        # TODO: check the data for inconsistencies
+                        opens, closes = None, None
+                    active_period.days.get_or_create(
+                        weekday=day['day'],
+                        opens=opens,
+                        closes=closes,
+                        closed=day['closed']
+                    )
