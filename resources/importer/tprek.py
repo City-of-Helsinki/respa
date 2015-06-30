c80f425f8083ccbf2d494a264ee1079d98a864ae
Juha Yrjölä <juha.yrjola@iki.fi>
2015-06-30 18:00:01 +0300

    tprek: Use only units from Helsinki

--- resources/importer/tprek.py
+++ resources/importer/tprek.py
@@ -56,7 +56,7 @@ class TPRekImporter(Importer):
         print("Fetching units")
         # 25480 == Public libraries
         # 25700 == Youth centers
-        url = "http://api.hel.fi/servicemap/v1/unit/?service=25480,25700&page_size=1000"
+        url = "http://api.hel.fi/servicemap/v1/unit/?service=25480,25700&municipality=helsinki&page_size=1000"
         resp = requests.get(url)
         assert resp.status_code == 200
         data = resp.json()
