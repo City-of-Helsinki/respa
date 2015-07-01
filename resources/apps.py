a9f8aa1383f91868585d5b7316091423454a3876
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-07-01 12:21:16 +0300

    Changed resource app's visible name to translated text

---
+++ resources/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+class ResourceConfig(AppConfig):
+    name = 'resources'
+    verbose_name = ugettext_lazy('Resource app')
+    #label = 'que'
+
+
