8a537c7c5a1755a4c643b4e64996e1b7d65109b1
Jussi Arpalahti <jussi.arpalahti@gmail.com>
2015-07-14 18:52:45 +0300

    Building new opening hours functionality with tests
    This can fetch open hours for all given resources at once
    Idea is to take only one or two db queries and cover
    possibly large set of resources (or all of them)
    
    Test case for this set up, but not yet working

--- resources/tests.py
+++ resources/tests.py
@@ -198,3 +198,39 @@ class ReservationApiTestCase(APITestCase):
         self.assertContains(response, '"ends":"' + res_start.isoformat())
         self.assertContains(response, '"starts":"' + res_end.isoformat())
         self.assertContains(response, '"ends":"' + end.isoformat())
+
+class PeriodTestCase(TestCase):
+
+    def __init__(self):
+        u1 = Unit.objects.create(name='Unit 1', id='unit_1')
+        u2 = Unit.objects.create(name='Unit 2', id='unit_2')
+        rt = ResourceType.objects.create(name='Type 1', id='type_1', main_type='space')
+        Resource.objects.create(name='Resource 1a', id='r1a', unit=u1, type=rt)
+        Resource.objects.create(name='Resource 1b', id='r1b', unit=u1, type=rt)
+        Resource.objects.create(name='Resource 2a', id='r2a', unit=u2, type=rt)
+        Resource.objects.create(name='Resource 2b', id='r2b', unit=u2, type=rt)
+
+        # Regular hours for one week
+        p1 = Period.objects.create(start=datetime.date(2015, 8, 3), end=datetime.date(2015, 8, 9), unit=u1, name='regular hours')
+        Day.objects.create(period=p1, weekday=0, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+        Day.objects.create(period=p1, weekday=1, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+        Day.objects.create(period=p1, weekday=2, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+        Day.objects.create(period=p1, weekday=3, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+        Day.objects.create(period=p1, weekday=4, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+        Day.objects.create(period=p1, weekday=5, opens=datetime.time(12, 0), closes=datetime.time(16, 0))
+        Day.objects.create(period=p1, weekday=6, opens=datetime.time(12, 0), closes=datetime.time(14, 0))
+
+        # Two shorter days as exception
+        exp1 = Period.objects.create(start=datetime.date(2015, 8, 6), end=datetime.date(2015, 8, 7), unit=u1,
+                                     name='exceptionally short days', exception=True, parent=p1)
+        Day.objects.create(period=exp1, weekday=3,
+                           opens=datetime.time(12, 0), closes=datetime.time(14, 0))
+        Day.objects.create(period=exp1, weekday=4,
+                           opens=datetime.time(12, 0), closes=datetime.time(14, 0))
+
+        # Weekend is closed as an exception
+        exp2 = Period.objects.create(start=datetime.date(2015, 8, 8), end=datetime.date(2015, 8, 9), unit=u1,
+            name='weekend is closed', closed=True, exception=True, parent=p1)
+
+    def test_all_periods(self):
+        pass
--- resources/timetools.py
+++ resources/timetools.py
@@ -1,1 +1,107 @@
-__author__ = 'jta'
+from .models import Day, Resource, Period, Unit, ResourceType
+from psycopg2.extras import DateTimeTZRange, DateRange, NumericRange
+import datetime
+import arrow
+from collections import namedtuple
+import django.db.models as dbm
+
+OpenHours = namedtuple("OpenHours", ['opens', 'closes'])
+
+def find_opening_hours(begin, end, resources=None):
+    """
+    :type begin:datetime
+    :type end:datetime
+    :type resources: Resource | None
+    :rtype: dict[datetime, dict[Resource, list[OpenHours]]]
+
+    Find opening hours for all resources on a given time period.
+
+    If resources is None, finds opening hours for all resources.
+
+    This version goes through all regular periods and then all
+    exception periods that are found overlapping the given
+    time range. It builds a dict of days that has dict of
+    resources with their active hours.
+
+    TODO: There is couple optimization avenues worth exploring
+    with prefetch or select_related for Periods'
+    relational fields Unit, Resource and Day. This way all
+    relevant information could be requested with one or two
+    queries from the db.
+    """
+    if not resources:
+        resources = Resource.objects.all()
+
+    d_range = DateRange(begin, end)
+
+    periods = Period.objects.filter(
+        dbm.Q(resource__in=resources) | dbm.Q(unit__in=resources.values("unit__pk")),
+        duration__overlap=d_range).order_by('exception')
+
+    begin_dt = datetime.datetime.combine(begin, datetime.time(0, 0))
+    end_dt = datetime.datetime.combine(end, datetime.time(0, 0))
+
+    # Generates a dict of time range's days as keys and values as active period's days
+    dates = {}
+    for period in periods:
+
+        if period.start < begin_dt.date():
+            start = begin_dt
+        else:
+            start = arrow.get(period.start)
+        if period.end > end_dt.date():
+            end = end_dt
+        else:
+            end = arrow.get(period.end)
+
+        if period.resource:
+            period_resources = [period.resource]
+        else:
+            period_resources = period.unit.resources.filter(pk__in=resources)
+
+        for res in period_resources:
+
+            for r in arrow.Arrow.range('day', start, end):
+                for day in period.days.all():
+                    if day.weekday is r.weekday():
+                        dates.setdefault(
+                            r.date(), {}).setdefault(
+                                res, []).append(
+                            OpenHours(day.opens, day.closes))
+
+    return dates
+
+
+def set():
+    u1 = Unit.objects.create(name='Unit 1', id='unit_1')
+    u2 = Unit.objects.create(name='Unit 2', id='unit_2')
+    rt = ResourceType.objects.create(name='Type 1', id='type_1', main_type='space')
+    Resource.objects.create(name='Resource 1a', id='r1a', unit=u1, type=rt)
+    Resource.objects.create(name='Resource 1b', id='r1b', unit=u1, type=rt)
+    Resource.objects.create(name='Resource 2a', id='r2a', unit=u2, type=rt)
+    Resource.objects.create(name='Resource 2b', id='r2b', unit=u2, type=rt)
+
+    # Regular hours for one week
+    p1 = Period.objects.create(start=datetime.date(2015, 8, 3), end=datetime.date(2015, 8, 9), unit=u1, name='regular hours')
+    Day.objects.create(period=p1, weekday=0, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+    Day.objects.create(period=p1, weekday=1, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+    Day.objects.create(period=p1, weekday=2, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+    Day.objects.create(period=p1, weekday=3, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+    Day.objects.create(period=p1, weekday=4, opens=datetime.time(8, 0), closes=datetime.time(18, 0))
+    Day.objects.create(period=p1, weekday=5, opens=datetime.time(12, 0), closes=datetime.time(16, 0))
+    Day.objects.create(period=p1, weekday=6, opens=datetime.time(12, 0), closes=datetime.time(14, 0))
+
+    # Two shorter days as exception
+    exp1 = Period.objects.create(start=datetime.date(2015, 8, 6), end=datetime.date(2015, 8, 7), unit=u1,
+                                 name='exceptionally short days', exception=True, parent=p1)
+    Day.objects.create(period=exp1, weekday=3,
+                       opens=datetime.time(12, 0), closes=datetime.time(14, 0))
+    Day.objects.create(period=exp1, weekday=4,
+                       opens=datetime.time(12, 0), closes=datetime.time(14, 0))
+
+    # Weekend is closed as an exception
+    exp2 = Period.objects.create(start=datetime.date(2015, 8, 8), end=datetime.date(2015, 8, 9), unit=u1,
+                name='weekend is closed', closed=True, exception=True, parent=p1)
+
+if __name__ == "__main__":
+    d = find_opening_hours(datetime.date(2015,8,1),datetime.date(2015,8,10))
