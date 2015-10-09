7fa85db4b148629afa40f71eb15a68a2e901f9db
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-10-09 16:47:29 +0300

    Importing local_settings works now

--- respa/settings.py
+++ respa/settings.py
@@ -135,6 +135,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
