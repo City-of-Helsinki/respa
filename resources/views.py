4f62143751fe3e3dd8aec203f7d9cb05cec5dd9e
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-10-15 00:10:21 +0300

    Easier testing stuff for availability check

--- resources/views.py
+++ resources/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from .models import Resource, Day
+from .models import Resource, Day, Unit
 import django.db.models as dbm
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 
+import resources.timetools
+import resources.models
+import datetime
+import pprint
 
-def testink(start_date, end_date):
+def testink(request):
     """
     Testing various ways of getting to resources by their availability time
     This function gets you active period for all resources in given date range
@@ -16,11 +21,24 @@ def testink(start_date, end_date):
     :param start_date, end_date:
     :return: most specific Resource
     """
+    start_date = request.GET.get('start_date', '2015-03-02')
+    end_date = request.GET.get('end_date', '2015-03-07')
+    duration = request.GET.get('duration', 2)
+    begin = datetime.datetime(2015,3,2)
+    end = datetime.datetime(2015,3,7)
+    duration = datetime.timedelta(hours=2)
+    openings, avail = resources.timetools.get_availability(begin, end, duration=duration)
 
-    Day.objects.filter(weekday__in=(1,2), closed=False)
+    return HttpResponse('<html><body>opens<br><pre>' + pprint.pformat(openings.values()) + '</pre> avail<br>' +
+                        '<pre>' + pprint.pformat(avail.values()) + '</pre></body></html>')
 
-    res = Resource.objects.filter(
-        periods__start__lte=start_date, periods__end__gte=end_date).annotate(
-        periods_length=dbm.F('periods__end')-dbm.F('periods__start')
-    ).order_by('periods_length').distinct()
-    return res
+def tester():
+
+    begin = datetime.datetime(2015,3,2)
+    end = datetime.datetime(2015,3,7)
+    begin_utc = resources.timetools.TimeWarp(dt=begin).astimezone('UTC')
+    end_utc = resources.timetools.TimeWarp(dt=end).astimezone('UTC')
+    duration = datetime.timedelta(hours=2)
+    openings, avail = resources.timetools.get_availability(begin_utc, end_utc, duration=duration)
+
+    return openings, avail
