0a0e22bcc2c17919f39880d905269ae3aafdbb04
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-10-19 13:49:56 +0300

    Importing local_settings relative to settings.py

--- respa/settings.py
+++ respa/settings.py
@@ -156,7 +156,7 @@ THUMBNAIL_PROCESSORS = (
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
 
