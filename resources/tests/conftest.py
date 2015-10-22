bd932333280b1e9c59cb19bac224edc6fee84eec
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-10-22 11:17:36 +0300

    Create fixtures for test resources

--- resources/tests/conftest.py
+++ resources/tests/conftest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
+from resources.models import ResourceType, Resource
 from rest_framework.test import APIClient, APIRequestFactory
 
 
@@ -11,3 +12,13 @@ def api_client():
 @pytest.fixture
 def api_rf():
     return APIRequestFactory()
+
+
+@pytest.fixture
+def space_resource_type():
+    return ResourceType.objects.get_or_create(id="test_space", name="test_space", main_type="space")[0]
+
+
+@pytest.fixture
+def space_resource(space_resource_type):
+    return Resource.objects.create(type=space_resource_type, authentication="none", name="resource")
--- resources/tests/test_resource.py
+++ resources/tests/test_resource.py
@@ -6,18 +6,16 @@ from resources.tests.utils import create_resource_image
 
 
 @pytest.mark.django_db
-def test_only_one_main_image():
-    restype = ResourceType.objects.create()
-    res = Resource.objects.create(type=restype, authentication="none", name="resource")
-    i1 = create_resource_image(res, type="main")
+def test_only_one_main_image(space_resource):
+    i1 = create_resource_image(space_resource, type="main")
     assert i1.type == "main"
-    i2 = create_resource_image(res, type="main")
+    i2 = create_resource_image(space_resource, type="main")
 
     assert i2.type == "main"
     # The first image should have been turned non-main after the new main image was created
     assert ResourceImage.objects.get(pk=i1.pk).type == "other"
 
-    i3 = create_resource_image(res, type="other")
+    i3 = create_resource_image(space_resource, type="other")
     assert i3.type == "other"
     # But adding a new non-main image should not have dethroned i2 from being main
     assert ResourceImage.objects.get(pk=i2.pk).type == "main"
