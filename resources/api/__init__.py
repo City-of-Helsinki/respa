6a713a36512aab5158c3c3e8d05b0d959bd6a16a
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-10-26 11:15:17 +0200

    Initialize API router in API package, not in project

--- resources/api/__init__.py
+++ resources/api/__init__.py
@@ -2,3 +2,24 @@ from .base import all_views
 from .resource import ResourceListViewSet, ResourceViewSet, PurposeViewSet
 from .reservation import ReservationViewSet
 from .unit import UnitViewSet
+
+from rest_framework import routers
+
+
+class RespaAPIRouter(routers.DefaultRouter):
+    def __init__(self):
+        super(RespaAPIRouter, self).__init__()
+        self.registered_api_views = set()
+        self.register_views()
+
+    def register_views(self):
+        for view in all_views:
+            kwargs = {}
+            if view['class'] in self.registered_api_views:
+                continue
+
+            self.registered_api_views.add(view['class'])
+
+            if 'base_name' in view:
+                kwargs['base_name'] = view['base_name']
+            self.register(view['name'], view['class'], **kwargs)
