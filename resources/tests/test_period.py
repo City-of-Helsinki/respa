a8a60e69952feb35d3fd43101da5076345f1c4f0
Aarni Koskela <aarni.koskela@andersinno.fi>
2015-10-28 17:14:52 +0200

    Test all of the Period validation cases

--- resources/tests/test_period.py
+++ resources/tests/test_period.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from datetime import date
+from datetime import date, timedelta
 import pytest
+
+from django.core.exceptions import ValidationError
 from resources.models import Period
 
 
@@ -10,3 +12,112 @@ def test_period_can_be_resaved(space_resource):
     period.save()
     period.name += "too"
     period.save()
+
+
+@pytest.mark.django_db
+def test_invalid_date_range(space_resource):
+    period = Period(resource=space_resource, start=date(2016, 8, 1), end=date(2015, 11, 1), name="test")
+    with pytest.raises(ValidationError) as ei:
+        period.full_clean()
+    assert ei.value.code == "invalid_date_range"
+
+
+@pytest.mark.django_db
+def test_invalid_belongings(space_resource, test_unit):
+    period = Period(start=date(2015, 8, 1), end=date(2015, 11, 1), name="test")
+    with pytest.raises(ValidationError) as ei:
+        period.full_clean()
+    assert ei.value.code == "no_belonging"
+
+    period.resource = space_resource
+    period.unit = test_unit
+    with pytest.raises(ValidationError) as ei:
+        period.full_clean()
+    assert ei.value.code == "invalid_belonging"
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("offsets", [
+    (-120, -120),  # overlap at the start of the big period
+    (+120, +120),  # overlap at the end of the big period
+    (+120, -120),  # overlap within the big period
+    (-120, +120),  # overlap outside the big period
+], ids=["start", "end", "within", "outside"])
+def test_period_overlaps(space_resource, offsets):
+    big_period = Period.objects.create(
+        resource=space_resource, start=date(2015, 1, 1), end=date(2015, 12, 1), name="test"
+    )
+    start_days, end_days = offsets
+    overlap_period = Period(
+        resource=space_resource,
+        start=big_period.start + timedelta(days=start_days),
+        end=big_period.end + timedelta(days=end_days),
+        name="overlap"
+    )
+    with pytest.raises(ValidationError) as ei:
+        overlap_period.full_clean()
+    assert ei.value.code == "overlap"
+
+
+@pytest.mark.django_db
+def test_multiple_exceptional_periods(space_resource):
+    Period.objects.create(
+        resource=space_resource, start=date(2015, 1, 1), end=date(2015, 12, 1), name="test"
+    )
+    Period.objects.create(
+        resource=space_resource,
+        start=date(2015, 6, 1),
+        end=date(2015, 8, 1),
+        name="summer",
+        exception=True
+    )
+    with pytest.raises(ValidationError) as ei:
+        Period(
+            resource=space_resource,
+            start=date(2015, 7, 1),
+            end=date(2015, 7, 15),
+            name="going_fishing",
+            exception=True
+        ).full_clean()
+    assert ei.value.code == "multiple_exceptions"
+
+
+@pytest.mark.django_db
+def test_larger_exceptional_period(space_resource):
+    Period.objects.create(
+        resource=space_resource, start=date(2015, 1, 1), end=date(2015, 12, 1), name="test"
+    )
+    with pytest.raises(ValidationError) as ei:
+        Period(
+            resource=space_resource, start=date(2014, 1, 1), end=date(2016, 12, 1), name="test", exception=True
+        ).full_clean()
+    assert ei.value.code == "larger_exception_than_parent"
+
+
+@pytest.mark.django_db
+def test_exceptional_period_exceptioning_multiple_periods(space_resource):
+    Period.objects.create(resource=space_resource, start=date(2015, 1, 1), end=date(2015, 7, 1), name="test1")
+    Period.objects.create(resource=space_resource, start=date(2015, 7, 1), end=date(2015, 12, 1), name="test2")
+
+    with pytest.raises(ValidationError) as ei:
+        Period(
+            resource=space_resource,
+            start=date(2015, 6, 1),
+            end=date(2015, 8, 1),
+            name="summer",
+            exception=True
+        ).full_clean()
+    assert ei.value.code == "exception_for_multiple_periods"
+
+
+@pytest.mark.django_db
+def test_exceptional_period_without_regular_period(space_resource):
+    with pytest.raises(ValidationError) as ei:
+        Period(
+            resource=space_resource,
+            start=date(2015, 6, 1),
+            end=date(2015, 8, 1),
+            name="summer",
+            exception=True
+        ).full_clean()
+    assert ei.value.code == "no_regular_period"
