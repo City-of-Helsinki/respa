fe1732cd574aba8080ca3b2fbd024f91e1943286
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-04-14 17:18:27 +0300

    Allow DI for Exchange session class, for testing

--- respa_exchange/session.py
+++ respa_exchange/session.py
@@ -1,11 +1,18 @@
 from django.conf import settings
-
-from respa_exchange.ews.session import ExchangeSession
+from django.utils.module_loading import import_string
 
 
 def get_ews_session():
+    """
+    Get a configured EWS session.
+
+    :rtype:   respa_exchange.ews.session.ExchangeSession
+    """
     # TODO: Maybe cache or something? NTLM handshakes can take some time...
-    return ExchangeSession(
+    session_class = import_string(
+        getattr(settings, "RESPA_EXCHANGE_EWS_SESSION_CLASS", "respa_exchange.ews.session.ExchangeSession")
+    )
+    return session_class(
         url=settings.RESPA_EXCHANGE_EWS_URL,
         username=settings.RESPA_EXCHANGE_EWS_USERNAME,
         password=settings.RESPA_EXCHANGE_EWS_PASSWORD,
