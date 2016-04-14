26a975a2e755c858ce43cfd392bdf834eb3510ed
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-04-14 15:40:48 +0300

    Wire up Exchange create/delete events with signals
    Refs City-of-Helsinki/respa-exchange#2
    
    Refs City-of-Helsinki/respa-exchange#4

--- respa_exchange/apps.py
+++ respa_exchange/apps.py
@@ -1,6 +1,21 @@
 from django.apps.config import AppConfig
+from django.db.models.signals import post_save, pre_delete
+
+from respa_exchange.signals import handle_reservation_save, handle_reservation_delete
 
 
 class RespaExchangeAppConfig(AppConfig):
     name = 'respa_exchange'
     verbose_name = 'Respa-Exchange'
+
+    def ready(self):
+        post_save.connect(
+            handle_reservation_save,
+            sender='resources.Reservation',
+            dispatch_uid='respa-exchange-save'
+        )
+        pre_delete.connect(
+            handle_reservation_delete,
+            sender='resources.Reservation',
+            dispatch_uid='respa-exchange-delete'
+        )
---
+++ respa_exchange/signals.py
@@ -0,0 +1,23 @@
+def handle_reservation_save(instance, **kwargs):
+    from respa_exchange.models import ExchangeResource, ExchangeReservation
+    exchange_reservation = ExchangeReservation.objects.filter(reservation=instance).first()
+    if not exchange_reservation:  # First sync? How exciting!
+        exchange_resource = ExchangeResource.objects.filter(sync_from_respa=True, resource=instance.resource).first()
+        if not exchange_resource:  # Not an Exchange-enabled resource; never mind.
+            return
+        exchange_reservation = ExchangeReservation(
+            reservation=instance,
+            principal_email=exchange_resource.principal_email
+        )
+
+        exchange_reservation.create_on_remote()
+    else:
+        exchange_reservation.update_on_remote()
+
+
+def handle_reservation_delete(instance, **kwargs):
+    from respa_exchange.models import ExchangeReservation
+    exchange_reservation = ExchangeReservation.objects.filter(reservation=instance).first()
+    if exchange_reservation:
+        exchange_reservation.delete_on_remote()
+        assert not exchange_reservation.pk
