2f8026263e8dd2fe66d6530322057f6f9a31e415
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-10-18 16:48:45 +0300

    Translation export args validation and file writing
    Still doing only basic CSV

--- resources/management/commands/translated_fields_export.py
+++ resources/management/commands/translated_fields_export.py
@@ -18,6 +18,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--models', type=str, nargs='*', help="Export given models")
         parser.add_argument('--all', action='store_true', help='Export all models')
+        parser.add_argument('--out', type=str, nargs=1, help='Output file name')
 
     def handle(self, *args, **options):
 
@@ -26,19 +27,38 @@ class Command(BaseCommand):
 
         resp = []
 
+        if not options.get('out'):
+            raise CommandError("Output file name required")
+
+        if options.get('all') and options.get('models'):
+            raise CommandError("Use only all or models options, not both")
+
         if options.get('all'):
             models = self.export_models
         else:
             models = options.get('models')
+
+        # Validate models
+        for model_name in models:
+            try:
+                apps.get_model('resources', model_name)
+            except Exception as e:
+                raise CommandError(("Problem finding model '%s': " % model_name) + str(e))
 
         for model_name in models:
             model = apps.get_model('resources', model_name)
             trans_opts = translator.get_options_for_model(model)
             translated_fields = sorted([tr_field.name for field in trans_opts.fields.values() for tr_field in field])
 
-            resp.append(['id'] + translated_fields)
+            resp.append(['"id"'] + ['"{}"'.format(f) for f in translated_fields])
 
             for obj in model.objects.all():
-                resp.append([obj.pk] + [getattr(obj, field) or '' for field in translated_fields])
+                resp.append([obj.pk] + ['"{}"'.format(getattr(obj, field) or '') for field in translated_fields])
 
-        print('\n'.join([';'.join(i) for i in resp]))
+        doc = open(options.get('out')[0], 'w+')
+        try:
+            doc.write('\n'.join([';'.join(i) for i in resp]))
+        except (IOError, OSError) as e:
+            print("Problems with file production", e)
+        finally:
+            doc.close()
