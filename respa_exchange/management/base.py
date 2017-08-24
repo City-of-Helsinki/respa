34d5cc0f52396aa09d75efbc7e7478218e367f7c
Juha Yrjölä <juha.yrjola@iki.fi>
2017-08-24 15:05:16 +0300

    Log respa_exchange management command exceptions with Raven

--- respa_exchange/management/base.py
+++ respa_exchange/management/base.py
@@ -2,6 +2,7 @@ import logging
 import sys
 
 from django.core.management import CommandError
+from django.conf import settings
 
 from respa_exchange.models import ExchangeResource
 
@@ -38,6 +39,14 @@ def configure_logging(logger="respa_exchange", level=logging.INFO, handler=None)
         datefmt=logging.Formatter.default_time_format
     ))
     logger.addHandler(handler)
+    if hasattr(settings, 'RAVEN_CONFIG') and 'dsn' in settings.RAVEN_CONFIG:
+        from raven.handlers.logging import SentryHandler
+        from raven.conf import setup_logging
+
+        sentry_handler = SentryHandler(settings.RAVEN_CONFIG['dsn'])
+        sentry_handler.setLevel(logging.ERROR)
+        logger.addHandler(sentry_handler)
+        setup_logging(sentry_handler)
 
 
 def select_resources(resources, selected_resources):
