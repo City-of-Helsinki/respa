f404f235587adaf42b63b040cf509b54f5c416c9
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2018-05-29 15:05:39 +0300

    Remove unnecessary deploy/local_settings

--- deploy/containerize.sh
+++ deploy/containerize.sh
@@ -27,5 +27,3 @@ docker tag "$REPO:$COMMIT" "$REPO:travis-$TRAVIS_BUILD_NUMBER"
 docker push "$REPO:$COMMIT"
 docker push "$REPO:travis-$TRAVIS_BUILD_NUMBER"
 docker push "$REPO:$BRANCH"
-
-env
--- deploy/local_settings.py
+++
@@ -1,8 +0,0 @@
-
-DATABASE = {
-	'default': {
-	        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-	        'NAME': 'respa',
-	        'HOST': '127.0.0.1'
-	    }
-}
