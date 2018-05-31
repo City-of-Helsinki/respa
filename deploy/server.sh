780f6ef0e78847142fa6a6e126ffb048766fba32
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2018-05-31 12:51:23 +0300

    Serve static files and work behind proxy and https

--- deploy/server.sh
+++ deploy/server.sh
@@ -4,4 +4,4 @@ echo "NOTICE: Get static files for serving"
 ./manage.py collectstatic --no-input
 
 echo "NOTICE: Start the gunicorn web server"
-gunicorn --access-logfile - --error-logfile - -b 0.0.0.0:8000 respa.wsgi
+gunicorn --access-logfile - --error-logfile - -b 0.0.0.0:8000 deploy.wsgi
