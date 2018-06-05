de1c895ee27fae86d355094e8dbf63f063111fc8
Jesse Laukkanen <jesse.laukkanen@anders.fi>
2018-06-05 15:49:02 +0300

    RespaAdmin: Fix urls

--- respa_admin/urls.py
+++ respa_admin/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.admin_index, name='index'),
     url(r'^resources/$', views.ResourceListView.as_view(), name='resources'),
-    url(r'^resource/$', views.admin_resource, name='resource'),
+    url(r'^resource/$', views.admin_form, name='resource'),
     url(r'^office/$', views.admin_office, name='office'),
     url(r'^form/$', views.admin_form, name='form')
 ]
