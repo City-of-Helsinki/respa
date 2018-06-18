bf0882acaf7a84ad64616dc761689040dfa690f6
Thanh Do <thanh.do@digia.com>
2018-06-18 22:57:08 +0300

    initial js, enable to close notification panel
    Signed-off-by: Thanh Do <thanh.do@digia.com>

--- respa_admin/static_src/js/index.js
+++ respa_admin/static_src/js/index.js
@@ -1,1 +1,16 @@
 import '../styles/base.scss';
+
+function start() {
+    enableNotificationHandling();
+}
+
+function exit() {}
+
+function enableNotificationHandling() {
+    let notifications = document.getElementsByClassName('noti');
+    Array.prototype.forEach.call(notifications, (noti) => {
+        noti.getElementsByTagName('button')[0].addEventListener('click', () => noti.remove(), false);
+    });
+}
+
+window.addEventListener('load', start, false);
