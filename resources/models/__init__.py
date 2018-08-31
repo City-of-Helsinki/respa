efa14404860a7b03b582f5c8b4933c98190004e2
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-08-31 16:42:46 +0300

    resources.models: Remove noqas in favor of __all__

--- resources/models/__init__.py
+++ resources/models/__init__.py
@@ -1,8 +1,31 @@
-from .availability import Day, Period, get_opening_hours  # noqa
-from .reservation import ReservationMetadataField, ReservationMetadataSet, Reservation, RESERVATION_EXTRA_FIELDS  # noqa
+from .availability import Day, Period, get_opening_hours
+from .reservation import ReservationMetadataField, ReservationMetadataSet, Reservation, RESERVATION_EXTRA_FIELDS
 from .resource import (
     Purpose, Resource, ResourceType, ResourceImage, ResourceEquipment, ResourceGroup,
     ResourceDailyOpeningHours, TermsOfUse
-)  # noqa
-from .equipment import Equipment, EquipmentAlias, EquipmentCategory  # noqa
-from .unit import Unit, UnitIdentifier  # noqa
+)
+from .equipment import Equipment, EquipmentAlias, EquipmentCategory
+from .unit import Unit, UnitIdentifier
+
+__all__ = [
+    'Day',
+    'Equipment',
+    'EquipmentAlias',
+    'EquipmentCategory',
+    'Period',
+    'Purpose',
+    'RESERVATION_EXTRA_FIELDS',
+    'Reservation',
+    'ReservationMetadataField',
+    'ReservationMetadataSet',
+    'Resource',
+    'ResourceDailyOpeningHours',
+    'ResourceEquipment',
+    'ResourceGroup',
+    'ResourceImage',
+    'ResourceType',
+    'TermsOfUse',
+    'Unit',
+    'UnitIdentifier',
+    'get_opening_hours',
+]
