b252f032544d8e064c343f0e00c6d7a9f60af862
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-09-06 10:17:47 +0300

    Add tests for the sanitizer functions

--- sanitizers/respa_string.py
+++ sanitizers/respa_string.py
@@ -4,6 +4,20 @@ _WORD_CHAR_RX = re.compile(r'\w')
 
 
 def sanitize_replace_with_xxx(value):
+    """
+    Sanitize a string value by replacing it with x's.
+
+    >>> sanitize_replace_with_xxx('')
+    ''
+
+    >>> sanitize_replace_with_xxx(None)
+
+    >>> sanitize_replace_with_xxx('Hello')
+    'xxxxx'
+
+    >>> sanitize_replace_with_xxx('Hello Sanitized World!')
+    'xxxxx xxxxxxxxx xxxxx!'
+    """
     if not value:
         return value
     return _WORD_CHAR_RX.sub('x', value)
--- sanitizers/respa_user.py
+++ sanitizers/respa_user.py
@@ -2,6 +2,22 @@ from database_sanitizer.sanitizers import user as base
 
 
 def sanitize_username(value):
+    """
+    Sanitize Respa username.
+
+    None, empty sting and AnonymousUser will be returned as is.
+    Otherwise a sanitized value is generated.
+
+    >>> sanitize_username('')
+    ''
+
+    >>> sanitize_username(None)
+
+    >>> sanitize_username('AnonymousUser')
+    'AnonymousUser'
+
+    >>> assert sanitize_username('john-doe') != 'john-doe'
+    """
     if not value or value == 'AnonymousUser':
         return value
     return base.sanitize_username(value)
