0e4326f019a8814b6f5a53fdd1ce7d5473a6f55d
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-09-11 14:25:35 +0300

    Rename static/dist to static/respa_admin
    The dist word is just too generic and doesn't give any hint that these
    are respa_admin styles or JS files.

--- respa_admin/webpack.config.js
+++ respa_admin/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   output: {
     filename: '[name]-bundle.js',
-    path: path.resolve(__dirname, './static/dist/'),
+    path: path.resolve(__dirname, './static/respa_admin/'),
   },
   module: {
     // Add loader
