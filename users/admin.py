e43f7df7b83b51f448fc7b82f8d65dde7bee0e37
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-09-27 12:19:01 +0300

    users: Improve Django Admin UX for Users
    Improve the user experience of managing User objects in Django Admin by
    making the "General Administrator" field visible and more fields visible
    in the user list view.  Also improve the Finnish transaltions of the
    fields and their help texts.

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,37 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
 
 User = get_user_model()
 
 
+def _add_general_admin_to_fieldsets(fieldsets):
+    def modify_field_data(field_data):
+        if 'is_superuser' in (field_data or {}).get('fields', ()):
+            fields = list(field_data['fields'])
+            fields.insert(fields.index('is_superuser'), 'is_general_admin')
+            return dict(field_data, fields=tuple(fields))
+        return field_data
+
+    return tuple(
+        (label, modify_field_data(field_data))
+        for (label, field_data) in fieldsets)
+
+
 class UserAdmin(DjangoUserAdmin):
-    fieldsets = DjangoUserAdmin.fieldsets + (
+    fieldsets = _add_general_admin_to_fieldsets(DjangoUserAdmin.fieldsets) + (
         (None, {'fields': ('department_name', 'uuid', 'favorite_resources')}),
     )
+    list_display = [
+        'uuid', 'username', 'email',
+        'first_name', 'last_name',
+        'is_staff', 'is_general_admin', 'is_superuser'
+    ]
+    list_filter = [
+        'is_staff', 'is_general_admin', 'is_superuser',
+        'is_active',
+        'groups',
+    ]
+
 
 admin.site.register(User, UserAdmin)
