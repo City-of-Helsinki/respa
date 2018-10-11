0afbd659b925b2681b92aeedf1568c3e943d1204
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2018-10-11 16:06:27 +0300

    Standardize container http server port

--- deploy/server.sh
+++ deploy/server.sh
@@ -4,4 +4,4 @@ echo "NOTICE: Get static files for serving"
 ./manage.py collectstatic --no-input
 
 echo "NOTICE: Start the uwsgi web server"
-uwsgi --http :8080 --wsgi-file deploy/wsgi.py --check-static /usr/src/app/www
+exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --check-static /usr/src/app/www
