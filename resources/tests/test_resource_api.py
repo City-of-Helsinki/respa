7f54340c7eb993e19d73b0cb68700341043b64d8
otiala <44601448+otiala@users.noreply.github.com>
2019-01-25 11:21:35 +0200

    Add municipality filter to ResourceFilterSet (#420)
    * Add municipality filter to ResourceFilterSet
    
    * Implement unit tests for municipality filter
    
    * Change municipality filter to be more explicit
    
    * Update swagger documentation

--- resources/tests/test_resource_api.py
+++ resources/tests/test_resource_api.py
@@ -674,3 +674,17 @@ def test_available_between_with_period(list_url, resource_in_unit, resource_in_u
     response = user_api_client.get(list_url, params)
     assert response.status_code == 200
     assert_response_objects(response, expected_resources)
+
+
+@pytest.mark.django_db
+def test_filtering_by_municipality(list_url, api_client, resource_in_unit, test_unit, test_municipality):
+    test_unit.municipality = test_municipality
+    test_unit.save()
+
+    response = api_client.get('%s?municipality=foo' % list_url)
+    assert response.status_code == 200
+    assert_response_objects(response, resource_in_unit) 
+
+    response = api_client.get('%s?municipality=bar' % list_url)
+    assert response.status_code == 200
+    assert_response_objects(response, []) 
