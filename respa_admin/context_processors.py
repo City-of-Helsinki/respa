0824aae29c69c7033d3139f9dd6679063a7e27f5
Oswald <oskari.tiala@haltu.fi>
2019-03-07 13:34:04 +0200

    Change instructions variable to me more specific

--- respa_admin/context_processors.py
+++ respa_admin/context_processors.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 def export_global_vars(request):
     data = {}
-    data['INSTRUCTIONS_URL'] = settings.RESPA_ADMIN_INSTRUCTIONS_URL
+    data['RESPA_ADMIN_INSTRUCTIONS_URL'] = settings.RESPA_ADMIN_INSTRUCTIONS_URL
     return data
