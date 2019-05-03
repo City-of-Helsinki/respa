e35d9400633d29586d9be1b8d8699e60baf74baa
Markus Salo <markus.salo@anders.fi>
2019-05-03 16:11:00 +0300

    Tweak active payment provider selection, configuration and usage
    Move configuration away from global respa settings to the payment provider implementation and init.
    Remove passing customer and product data through generic helpers at least for now.
    Split the payment initialization so that the order create response handling can be tested without actually making network requests.
    Add tests for different order create responses.

--- payments/integrations/__init__.py
+++ payments/integrations/__init__.py
@@ -1,12 +1,33 @@
-from .bambora_payform import BamboraPayformPayments
+import environ
 
+from django.conf import settings
+from django.utils.module_loading import import_string
 
 _payment_provider = None
 
 
 def get_payment_provider():
+    """Load and cache the active payment provider"""
     global _payment_provider
     if not _payment_provider:
-        # TODO where/how should we choose this?
-        _payment_provider = BamboraPayformPayments()
+
+        # Provider path is the only thing loaded from env
+        # in the global settings, the rest are added here
+        provider_path = getattr(settings, 'RESPA_PAYMENTS_PROVIDER')
+        provider = import_string(provider_path)
+
+        # Provider tells what keys and types it requires for configuration
+        # and the corresponding data has to be set in .env
+        template = provider.get_config_template()
+        env = environ.Env(**template)
+
+        config = {}
+        for key in template.keys():
+            if hasattr(settings, key):
+                config[key] = getattr(settings, key)
+            else:
+                config[key] = env(key)
+
+        _payment_provider = provider(PAYMENT_CONFIG=config)
+
     return _payment_provider
