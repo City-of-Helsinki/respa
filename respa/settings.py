6abb398fcdd24f1ddaa73f9856f5b3d82d4280e6
Tuomas Haapala <tuomas.haapala@anders.fi>
2019-05-11 22:10:45 +0300

    Add products field to resource API data

--- respa/settings.py
+++ respa/settings.py
@@ -293,6 +293,12 @@ if os.path.exists(local_settings_path):
         code = compile(fp.read(), local_settings_path, 'exec')
     exec(code, globals(), locals())
 
+
+if 'payments' in INSTALLED_APPS:
+    RESPA_RESOURCE_SERIALIZER_CLASS = 'payments.api.ResourceSerializer'
+    RESPA_RESOURCE_DETAILS_SERIALIZER_CLASS = 'payments.api.ResourceDetailsSerializer'
+
+
 # If a secret key was not supplied from elsewhere, generate a random one
 # and store it into a file called .django_secret.
 if 'SECRET_KEY' not in locals():
