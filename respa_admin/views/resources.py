0728c958def6a5081844f231b434f4a826cb7c9f
Jussi Heikkilä <jussi.heikkila@haltu.fi>
2019-05-27 12:00:40 +0300

    RESPA-91: Add unit.id as locationId to accessibility data input link. (#563)
    * Add unit.id as locationId to accessibility data input link. Strip tprek: prefix from unit id when enterin accessibility data.

--- respa_admin/views/resources.py
+++ respa_admin/views/resources.py
@@ -165,6 +165,7 @@ class SaveResourceView(ExtraContextMixin, CreateView):
         secret = getattr(settings, 'RESPA_ADMIN_ACCESSIBILITY_API_SECRET', '')
         target_id = self.object.pk
         target_name = self.object.name
+        location_id = str(self.object.unit.id).lstrip('tprek:')  # remove prefix, use bare tprek id
         user = request.user.email or request.user.username
         return accessibility_api.generate_url(
             api_url,
@@ -173,6 +174,7 @@ class SaveResourceView(ExtraContextMixin, CreateView):
             target_name,
             user,
             secret,
+            location_id=location_id
         )
 
     def post(self, request, *args, **kwargs):
