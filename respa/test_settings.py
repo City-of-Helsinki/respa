89c93d14b91fa369a756fe4875f46332f329fa1c
Tuomas Haapala <tuomas.haapala@anders.fi>
2019-06-11 22:43:40 +0300

    Remove obsolete code

--- respa/test_settings.py
+++ respa/test_settings.py
@@ -4,6 +4,3 @@ from .settings import *
 RESPA_CATERINGS_ENABLED = True
 RESPA_COMMENTS_ENABLED = True
 RESPA_PAYMENTS_ENABLED = True
-
-RESPA_RESOURCE_SERIALIZER_CLASS = 'payments.api.ResourceSerializer'
-RESPA_RESOURCE_DETAILS_SERIALIZER_CLASS = 'payments.api.ResourceDetailsSerializer'
