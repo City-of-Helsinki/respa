04f39efb15b4f4206a122e665f8377c7198e1f25
Jussi Heikkilä <jussi.heikkila@haltu.fi>
2019-06-24 15:32:35 +0300

    Unconfigured Respa Admin logo properly disabled. (#601)

--- respa_admin/views/base.py
+++ respa_admin/views/base.py
@@ -7,6 +7,7 @@ class ExtraContextMixin():
         context = super().get_context_data(**kwargs)
         context['INSTRUCTIONS_URL'] = settings.RESPA_ADMIN_INSTRUCTIONS_URL
         context['SUPPORT_EMAIL'] = settings.RESPA_ADMIN_SUPPORT_EMAIL
-        context['logo_url'] = staticfiles_storage.url('respa_admin/img/{0}'.format(settings.RESPA_ADMIN_LOGO))
+        if settings.RESPA_ADMIN_LOGO:
+            context['logo_url'] = staticfiles_storage.url('respa_admin/img/{0}'.format(settings.RESPA_ADMIN_LOGO))
         context['KORO_STYLE'] = settings.RESPA_ADMIN_KORO_STYLE
         return context
