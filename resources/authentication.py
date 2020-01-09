afb62be0049fac286ed71e6ee460021a1dcd4855
Oskari Tiala <oskari.tiala@haltu.fi>
2020-01-09 09:46:17 +0200

    Remove unused import and add newline to the end of the file

--- resources/authentication.py
+++ resources/authentication.py
@@ -1,10 +1,7 @@
 from django.utils.translation import ugettext as _
 from rest_framework import exceptions
-from rest_framework_jwt.settings import api_settings
 
 from helusers.jwt import JWTAuthentication
-
-jwt_get_username_from_payload = api_settings.JWT_PAYLOAD_GET_USERNAME_HANDLER
 
 
 class RespaJWTAuthentication(JWTAuthentication):
