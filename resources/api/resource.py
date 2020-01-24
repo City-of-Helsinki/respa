109d5f07ba20a11c36c83f46f3b4075adc933527
Tommi Mantila <tommi@manbytes.fi>
2020-01-24 15:36:12 +0200

    Added token and session authentications for single resource viewset

--- resources/api/resource.py
+++ resources/api/resource.py
@@ -18,7 +18,7 @@ from django.contrib.auth import get_user_model
 
 from resources.pagination import PurposePagination
 from rest_framework import exceptions, filters, mixins, serializers, viewsets, response, status
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.decorators import action
 from guardian.core import ObjectPermissionChecker
 
@@ -770,6 +770,9 @@ class ResourceListViewSet(munigeo_api.GeoModelAPIView, mixins.ListModelMixin,
 class ResourceViewSet(munigeo_api.GeoModelAPIView, mixins.RetrieveModelMixin,
                       viewsets.GenericViewSet, ResourceCacheMixin):
     queryset = ResourceListViewSet.queryset
+    authentication_classes = (
+        list(drf_settings.DEFAULT_AUTHENTICATION_CLASSES) +
+        [TokenAuthentication, SessionAuthentication])
 
     def get_serializer_class(self):
         if settings.RESPA_PAYMENTS_ENABLED:
