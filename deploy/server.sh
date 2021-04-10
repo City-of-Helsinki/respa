10ca513508cdcf2109fc5ff50dc80f36a0f62d20
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2021-04-10 16:57:40 +0300

    Add offload threads and cheaper worker limit
    Cheaper scales workers down to given number when usage is low

--- deploy/server.sh
+++ deploy/server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "NOTICE: Start the uwsgi web server"
-exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --static-map /${MOUNT_PATH}static=/usr/src/app/static --static-map /${MOUNT_PATH}media=/usr/src/app/www/media --log-master -p 10 -T --master --listen 2048 --static-gzip-all
+exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --static-map /${MOUNT_PATH}static=/usr/src/app/static --static-map /${MOUNT_PATH}media=/usr/src/app/www/media --master --log-master -p 8 -T --offload-threads 2 --cheaper 2 --listen 2048 --static-gzip-all
 
 # variables: port, media root, -p for processes, --listen for listening connections
