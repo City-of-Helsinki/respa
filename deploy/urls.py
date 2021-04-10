f2c5b86b1284d4aa2b8619f6ef703be2e3a690cd
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2021-04-10 14:31:38 +0300

    Use mount path in deploy urls.py

--- deploy/urls.py
+++ deploy/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path, re_path
 from django.conf.urls import include
+from django.conf import settings
 
 urlpatterns = [
-    path('', include('respa.urls')),
+    path(settings.MOUNT_PATH, include('respa.urls')),
 ]
