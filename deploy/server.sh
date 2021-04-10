2ad0d9418f0aeef2e13be4129045e22f2c903cf7
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2021-04-10 16:18:46 +0300

    Use Uwsgi for serving static files

--- deploy/server.sh
+++ deploy/server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "NOTICE: Start the uwsgi web server"
-exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --static-map /media=/usr/src/app/www/media --log-master -p 10 -T --master --listen 2048
+exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --static-map /static=/usr/src/app/static --static-map /media=/usr/src/app/www/media --log-master -p 10 -T --master --listen 2048 --static-gzip-all
 
 # variables: port, media root, -p for processes, --listen for listening connections
--- deploy/settings.py
+++ deploy/settings.py
@@ -1,12 +1,4 @@
 
 from respa.settings import *
 
-# Get whitenoise for serving static files
-try:
-    place = MIDDLEWARE.index('django.middleware.security.SecurityMiddleware')
-except ValueError:
-    place = 0
-
-MIDDLEWARE.insert(place, 'whitenoise.middleware.WhiteNoiseMiddleware')
-
 ROOT_URLCONF = 'deploy.urls'
