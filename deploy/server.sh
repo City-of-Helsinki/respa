b7b775a45680a5befa90295d8bedf693fe0c833e
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2021-04-11 17:44:41 +0300

    Remove server.sh in favor of direct Uwsgi start

--- deploy/server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "NOTICE: Start the uwsgi web server"
-exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --static-map /${MOUNT_PATH}static=/usr/src/app/static --static-map /${MOUNT_PATH}media=/usr/src/app/www/media --master --log-master -p 8 -T --offload-threads 2 --cheaper 2 --listen 2048 --static-gzip-all
-
-# variables: port, media root, -p for processes, --listen for listening connections
