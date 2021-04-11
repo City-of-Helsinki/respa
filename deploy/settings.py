726191a717f8dd7f55d1dc3fbea9331b08773356
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2021-04-11 17:46:23 +0300

    Add Whitenoise back for static files

--- deploy/settings.py
+++ deploy/settings.py
@@ -2,3 +2,12 @@
 from respa.settings import *
 
 ROOT_URLCONF = 'deploy.urls'
+
+try:
+    place = MIDDLEWARE.index('django.middleware.security.SecurityMiddleware')
+except ValueError:
+    place = 0
+
+MIDDLEWARE.insert(place, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
