11b1d1815fc2b0752b3ba13c4f935e29bee9c9df
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2021-04-14 13:06:44 +0300

    Add debug URL to deploy

--- deploy/settings.py
+++ deploy/settings.py
@@ -11,3 +11,8 @@ except ValueError:
 MIDDLEWARE.insert(place, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# To get Sentry report for URLs
+import environ
+env = environ.Env(DEBUG_REQUEST=(bool, False))
+DEBUG_REQUEST = env('DEBUG_REQUEST')
